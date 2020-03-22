b88bd3048eec63863b24822df54e80d5d58a35cc
Maffo102 <maffei.filippo102@gmail.com>
2020-03-22 08:18:44 -0400

    Emdee five for life CTF

--- htb/web/Emdee_five_for_life/emdee.py
+++ htb/web/Emdee_five_for_life/emdee.py
@@ -2,19 +2,22 @@ import requests
 import re
 import md5
 import hashlib
+
+
+
 s = requests.Session()
 r = s.get('http://docker.hackthebox.eu:32356/')
 print("Getting string to hash...")
+
 sth = r.text.split('\n')[5].split('<')[3].split('>')[1]
 print("The string is: " + sth)
+
 print("Calculating hash...")
 res = hashlib.md5(sth.encode('utf-8')).hexdigest()
 print("The hash is: {}".format(res))
+
+
 resp = {'hash': res}
 req2 = s.post('http://docker.hackthebox.eu:32356/', data=resp)
 print("The flag is: " + req2.text.split('\n')[5].split('<')[5].split('>')[1])
-
-
-
-
 
